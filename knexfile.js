3b794cef0d26d564c5b60fc9f139244f55b2c9f1
Chad Cox <chad@Chads-MacBook-Pro.local>
2021-01-05 10:44:26 -0700

    fixed knex

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,21 @@
-require('dotenv').config();
-
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/users";
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './database/users.db3'
+      filename: './database/recipes.db3'
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
     migrations: {
       directory: './database/migrations'
     },
     seeds: {
       directory: './database/seeds'
-    }
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
   },
-},
-
-production: {
-  client: "pg",
-  connection: pgConnection,
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    directory: "./database/migrations",
-  },
-  seeds: {
-    directory: "./database/seeds",
-  },
-},
 };
